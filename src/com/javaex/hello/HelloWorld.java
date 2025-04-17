a3e5d8dfeca8fc39746bf62dd1e4aeda8fed0760
jkm8369 <jkm8369@naver.com>
2025-04-17 09:49:56 +0900

    1st

---
+++ src/com/javaex/hello/HelloWorld.java
@@ -0,0 +1,15 @@
+/*작성자: 황일영
+작성일: 2024-04-14
+설 명: Hello World 출력하기
+*/
+
+package com.javaex.hello;
+
+public class HelloWorld{
+    
+    public static void main(String[] args){
+    	
+    	//문자열을 화면에 출력하는 함수
+        System.out.println("Hello World!");
+	}
+}
